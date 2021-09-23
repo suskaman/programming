93fb8b6d9c93602037f08fd81cae448a292a7438
suskaman <89830556+suskaman@users.noreply.github.com>
2021-09-23 22:55:39 +0300

    Add files via upload

---
+++ laba2.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	long float a, z1, z2;
+	scanf("%f", &a);
+
+	z1 = 1 - 1 / 4 * pow(sin(2 * a), 2) + cos(2 * a);
+	printf("%f\n", z1);
+
+	z2 = pow(cos(a), 2) + pow(cos(a), 4);
+
+	printf("%f", z2);
+	return 0;
+}
