1a6934a758752e45d524d29a88886ec46c62ad47
suskaman <89830556+suskaman@users.noreply.github.com>
2021-09-23 22:14:03 +0300

    Add files via upload

---
+++ laba.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int num;
+	char str[102] = "";
+
+	scanf("%d\n", &num);
+	fgets(str, 102, stdin);
+
+	printf("%d\n", num);
+	puts(str);
+	return 0;
+}
