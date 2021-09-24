d56a58f81ddc8e49326a4956caea8e37057d8ad9
suskaman <89830556+suskaman@users.noreply.github.com>
2021-09-24 10:32:58 +0300

    Update laba2.cpp

--- laba2.cpp
+++ laba2.cpp
@@ -4,14 +4,13 @@
 
 int main()
 {
-	long float a, z1, z2;
+	float a, z1, z2, z3, z4, z5;
 	scanf("%f", &a);
-
 	z1 = 1 - 1 / 4 * pow(sin(2 * a), 2) + cos(2 * a);
-	printf("%f\n", z1);
-
 	z2 = pow(cos(a), 2) + pow(cos(a), 4);
-
-	printf("%f", z2);
+	z3 = pow(a, 5);
+	z4 = cos(a);
+	z5 = sqrt(a);
+	printf("%f\n%f\n%f\n%f\n%f", z1,z2,z3,z4,z5);
 	return 0;
 }
