bf76d03aeef0b50727a44da69ab9db097d37de53
suskaman <89830556+suskaman@users.noreply.github.com>
2021-10-07 22:08:10 +0300

    Add files via upload

---
+++ laba3.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int numa, numb;
+	scanf("%d", &numa);
+	printf("%d\n", numa);
+	printf("%x\n", numa);
+	printf("%o\n", numa << 1);
+	printf("%x\n", ~numa);
+	scanf("%d", &numb);
+	printf("%o\n", numa ^ numb);
+	return 0;
+}
