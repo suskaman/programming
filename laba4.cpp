d0d46841249c1695bb10f3980379597319075e84
suskaman <89830556+suskaman@users.noreply.github.com>
2021-10-08 09:22:12 +0300

    Add files via upload

---
+++ laba4.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int num = 0, arg = 0, count = 0, n = 5;
+	scanf("%d", &num);
+	arg = (-3 < num) ? (num < 6) ? num : num - num: num - num;
+	printf("%d\n", arg);
+
+	for (int i = 0; i < 5; i++) {
+		if (i == 4) {
+			num = num % 2;
+		}
+		else {
+			num = num / 2;
+		}
+	}
+	printf("%d", num);
+	return 0;
+}
