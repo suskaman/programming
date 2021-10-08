3325b066bc8c7a02644e4e1bf8c4bd981d854a4c
suskaman <89830556+suskaman@users.noreply.github.com>
2021-10-08 10:32:16 +0300

    Update laba4.cpp

--- laba4.cpp
+++ laba4.cpp
@@ -1,11 +1,11 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main()
 {
-	int num = 0, arg = 0, count = 0, n = 5;
+	int num = 0, arg = 0;
 	scanf("%d", &num);
-	arg = (-3 < num) ? (num < 6) ? num : num - num: num - num;
+	arg = (-3 < num) ? (num < 6) ? num : 0: 0;
 	printf("%d\n", arg);
 	printf("%d", (num >> 5) % 2); 
 	return 0;
