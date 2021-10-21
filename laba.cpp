1a7b446047ee7f4eb8158ad28133314dc7b7315d
suskaman <89830556+suskaman@users.noreply.github.com>
2021-10-22 01:08:32 +0300

    Add files via upload

---
+++ laba.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <malloc.h>
+
+int main() {
+	char arr[] = "WORK";
+	char* ell;
+	ell = arr;
+	int n = sizeof(arr);
+	for (int i = 0; i < n; i++) {
+		printf("%c", ell[i]);
+		printf(" ");
+	}
+	printf("\n");
+	
+	char* temp = (char*)malloc(n*sizeof(int));
+
+	for (int i = 0; i < n; i++) {
+		temp[i] = ell[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		printf("%c", temp[i]);
+		printf(" ");
+	}
+	free(temp);
+	return 0;
+}
