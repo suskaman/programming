154313aad993a995f657e117f2861f99c6c76a16
suskaman <89830556+suskaman@users.noreply.github.com>
2021-11-05 11:59:13 +0300

    Update laba8.cpp

--- laba8.cpp
+++ laba8.cpp
@@ -6,19 +6,28 @@ int main() {
 	char copys[100];
 	char str[100];
 	char str_2[90];	
-	scanf("%[^\n]s\n", str);
-	scanf("%[^\n]s\n", str_2);
+	char symbol;
+	scanf(" %[^\n]s\n", str);
+    scanf(" %[^\n]s\n", str_2);
+
 	strcpy(copys, str);
 
 	printf("5 - copy - %s\n", copys);
 	printf("7 - length - %d\n", strlen(str));
 	printf("9 - search - %s\n", strrchr(str,'c'));
-	printf("11 - lengthchar - %d\n", strcspn(str,str_2));
-	printf("--13 - 13--\n");
-	char* temp = strtok(str, "-");
+	printf("11 - lengthchar - %d\n", strspn(str,str_2));
+	printf("13 - 13\n");
+
+	char sym[5];
+	char* temp;
+	scanf(" %c", sym);
+
+	temp = strtok(str, sym);
+
 	while (temp != NULL) {
 		printf("%s\n", temp);
-		temp = strtok(NULL,"-");
+		temp = strtok(NULL,sym);
 	}
+
 	return 0;
 }
