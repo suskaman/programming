64343140872d1d9be21ba3c501e844ad1943bee4
suskaman <89830556+suskaman@users.noreply.github.com>
2021-11-19 09:40:26 +0300

    Update laba10.cpp

--- laba10.cpp
+++ laba10.cpp
@@ -3,38 +3,26 @@
 #include<locale.h>
 #include<string.h>
 
-int NOD(int num1, int num2, int ans) {
-    int num;
-    if (num1 > num2) { num = num1; }
-    else { num = num2; }
-    for (int i = 1; i < num / 2 + 1; i++) {
-        if ((num1 % i == 0) and (num2 % i == 0)) {
-            ans = i;
+int NOD(int num1, int num2) {
+    while (num1 != num2){
+        if (num1 > num2){
+            num1 -= num2;
+        } else {
+            num2 -= num1;
         }
     }
-    return ans;
-}
-int NOK(int num1, int num2, int ans) {
-    bool Flag = true;
-    int num;
-    if (num1 > num2) { num = num1; }
-    else { num = num2; }
-    while (num > 0) {
-        num++;
-        if ((num % num1 == 0) and (num % num2 == 0)) {
-            Flag = false;
-            ans = num;
-            break;
-        }
-    }
-    return ans;
+    return num2;
 }
 
-void d(char* str, int j)
+int NOK(int num1, int num2){
+    return (num1 * num2)/NOD(num1, num2);
+}
+
+void delete_space(char* str, int j)
 {
     int i;
     if ((str[j] == ' ') and (str[j + 1]) == ' ')
-        d(str, j + 1);
+        delete_space(str, j + 1);
     for (i = j; i < strlen(str) - 1; i++)
     {
         str[i] = str[i + 1];
@@ -46,16 +34,16 @@ void d(char* str, int j)
 int main() {
     setlocale(LC_ALL, "Rus");
     //задание 1
-    int ans = 0, num1, num2;
+    int num1, num2;
     printf("Введите первое число: ");
     scanf(" %d", &num1);
     printf("Введите второе число: ");
     scanf(" %d", &num2);
 
-    int m = NOD(num1, num2, ans);
+    int m = NOD(num1, num2);
     printf("НОД: %d\n", m);
 
-    int n = NOK(num1, num2, ans);
+    int n = NOK(num1, num2);
     printf("НОК: %d\n", n);
     printf("-----------\n");
 
@@ -66,7 +54,7 @@ int main() {
     for (int i = 0; i < strlen(str); i++)
     {
         if ((str[i] == ' ') and (str[i + 1] == ' '))
-            d(str, i + 1);
+            delete_space(str, i + 1);
     }
     printf("%s\n", str);
     return 0;
