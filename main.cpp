e0c2f04b3ab04b252ed948b29dcc10334d70a979
suskaman <89830556+suskaman@users.noreply.github.com>
2021-12-03 01:02:12 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SUCURE_NO_WARNINGS
+#include<stdio.h>
+#include"my_rhomb.h"
+
+int main() {
+	struct rhomb value = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+	value = create_struct(value);
+	float perimeter = R_perimeter(value);
+	float square = R_square(value);
+	printf("SQUARE = %f\n", square);
+	printf("PERIMETER = %f", perimeter);
+	return 0;
+}
