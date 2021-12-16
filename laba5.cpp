78e367be59fd5ba4f5def0236cd54378603e1b2e
suskaman <89830556+suskaman@users.noreply.github.com>
2021-12-16 16:39:12 +0300

    Revert "Laba11"

---
+++ laba5.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(){
+	int arr[9] = {99, 88, 77, 66, 55, 44, 33, 22, 11};
+	int mat[2][2] = {1, 1, 1, 4};
+	int mat2[2][2] = {1, 2, 4, 1};
+	int ans[2][2] = {0};
+
+	printf("myarray:\n");
+	for (int i = 0; i < 9; i++) {
+		printf("%d", arr[i]);
+		printf(" ");
+	}
+	printf("\n");
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++) {
+			for (int k = 0; k < 2; k++) {
+				ans[i][j] += mat[i][k] * mat2[k][j];
+			}
+		}
+	}
+	for(int x = 0; x < 2; x++) {
+		for (int y = 0; y < 2; y++) {
+			printf("%d", ans[x][y]);
+			printf(" ");
+		}
+		for (int y = 0; y < 2; y++) {
+			printf("%d", ans[y][x]);
+			printf(" ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
---
+++ laba7.cpp
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+enum music
+{
+	classic,
+	pop,
+	rock,
+	rap,
+	new_age,
+	electro
+};
+
+struct geometry
+{
+	int X1, Y1;
+	int X2, Y2;
+};
+
+struct ADSL
+{
+	unsigned int DSL : 1;
+	unsigned int PPP : 1;
+	unsigned int Link : 1;
+};
+
+union myunion
+{
+	struct ADSL b;
+	short i;
+};
+
+int main() {
+	//задание 1
+	enum music a;
+	a = rock;
+	printf("%d\n", a);
+
+	//задание 2
+	struct geometry ans = { 1,1,5,1 };
+	float length;
+	int num = ans.X1 - ans.X2, num2 = ans.Y1 - ans.Y2;
+	length = sqrt(num * num + num2 * num2);
+	printf("%f\n", length);
+
+	// задание 3
+	union myunion t;
+	scanf("%x", &t.i);
+	printf("%d\n", t.b.DSL);
+	printf("%d\n", t.b.PPP);
+	printf("%d\n", t.b.Link);
+	return 0;
+}
