0718a0fdf026f18d9baef34ff6adff3408f92e5b
suskaman <89830556+suskaman@users.noreply.github.com>
2021-12-17 09:12:47 +0300

    Update main.cpp

--- laba11/main.cpp
+++ laba11/main.cpp
@@ -1,11 +1,26 @@
-
-#define _CRT_SUCURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include"my_rhomb.h"
 
 int main() {
 	struct rhomb value = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
-	value = create_struct(value);
+	printf("X1 = ");
+	scanf(" %f", value.x1);
+	printf("X2 = ");
+	scanf(" %f", value.x2);
+	printf("Y1 = ");
+	scanf(" %f", value.y1);
+	printf("Y2 = ");
+	scanf(" %f", value.y2);
+	printf("Z1 = ");
+	scanf(" %f", value.z2);
+	printf("Z2 = ");
+	scanf(" %f", value.z1);
+	printf("W1 = ");
+	scanf(" %f", value.w1);
+	printf("W2 = ");
+	scanf(" %f", value.w2);
+
 	float perimeter = R_perimeter(value);
 	float square = R_square(value);
 	printf("SQUARE = %f\n", square);
