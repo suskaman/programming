3c92f2d04f53669509fadb815f0b20181b6e1ace
suskaman <89830556+suskaman@users.noreply.github.com>
2021-12-17 09:08:17 +0300

    Update my_rhomb.h

--- laba11/my_rhomb.h
+++ laba11/my_rhomb.h
@@ -5,7 +5,6 @@ struct rhomb {
 	float x1, x2, y1, y2, z1, z2, w1, w2;
 };
 
-struct rhomb create_struct(struct rhomb value);
 float R_square(struct rhomb value);
 float R_perimeter(struct rhomb value);
 #endif // MY_RHOMB
