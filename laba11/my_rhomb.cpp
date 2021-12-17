e47b9230e007f820960c1d87b1f8ba4eed482587
suskaman <89830556+suskaman@users.noreply.github.com>
2021-12-17 09:07:43 +0300

    Update my_rhomb.cpp

--- laba11/my_rhomb.cpp
+++ laba11/my_rhomb.cpp
@@ -1,29 +1,17 @@
 #include<math.h>
 #include "my_rhomb.h"
 
-float R_square(struct rhomb coord) {
+float R_square(struct rhomb с) {
 	float BD, AC;
-	BD = sqrt(pow((coord.x1 - coord.z1) / 2, 2) + pow((coord.x2 - coord.z2) / 2, 2));
-	AC = sqrt(pow((coord.y1 - coord.w1) / 2, 2) + pow((coord.y2 - coord.w2) / 2, 2));
+	BD = sqrt(pow((с.x1 - с.z1), 2) + pow((с.x2 - с.z2), 2));
+	AC = sqrt(pow((с.y1 - с.w1), 2) + pow((с.y2 - с.w2), 2));
 	return  0.5 * AC * BD;
 }
-float R_perimeter(struct rhomb coord) {
+float R_perimeter(struct rhomb с) {
 	float AB, BC, CD, DA;
-	AB = sqrt(pow((coord.x1 - coord.y1) / 2, 2) + pow((coord.x2 - coord.y2) / 2, 2));
-	BC = sqrt(pow((coord.y1 - coord.z1) / 2, 2) + pow((coord.y2 - coord.z2) / 2, 2));
-	CD = sqrt(pow((coord.z1 - coord.w1) / 2, 2) + pow((coord.z2 - coord.w2) / 2, 2));
-	DA = sqrt(pow((coord.w1 - coord.x1) / 2, 2) + pow((coord.w2 - coord.x2) / 2, 2));
+	AB = sqrt(pow((с.x1 - с.y1), 2) + pow((с.x2 - с.y2), 2));
+	BC = sqrt(pow((с.y1 - с.z1), 2) + pow((с.y2 - с.z2), 2));
+	CD = sqrt(pow((с.z1 - с.w1), 2) + pow((с.z2 - с.w2), 2));
+	DA = sqrt(pow((с.w1 - с.x1), 2) + pow((с.w2 - с.x2), 2));
 	return AB + BC + CD + DA;
-}
-
-struct rhomb create_struct(struct rhomb coord) {
-	coord.x1 = 1;
-	coord.x2 = 6;
-	coord.y1 = 4;
-	coord.y2 = 10;
-	coord.z1 = 7;
-	coord.z2 = 6;
-	coord.w1 = 4;
-	coord.w2 = 2;
-	return coord;
 }
